e334b209b374c9dc904e388783ca091d31521f9b
PizzaLvr49 <jhtodd2012@gmail.com>
2026-06-03 19:13:21 -0400

    CI fixes yay

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,7 +1,15 @@
+
+#[cfg(feature = "render")]
 pub mod assets;
 pub mod ecs;
+
+#[cfg(feature = "render")]
 pub mod input;
+
+#[cfg(feature = "render")]
 pub mod physics;
+
+#[cfg(feature = "render")]
 pub mod renderer;
 pub mod scripting;
 
@@ -13,10 +21,14 @@ impl Plugin for CorePlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins((
             ecs::EcsPlugin,
+            #[cfg(feature = "render")]
             assets::AssetsPlugin,
+            #[cfg(feature = "render")]
             physics::PhysicsPlugin,
+            #[cfg(feature = "render")]
             input::InputPlugin,
             scripting::ScriptingPlugin,
+            #[cfg(feature = "render")]
             renderer::RendererPlugin,
         ));
     }
--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,18 @@ mod core;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                title: String::from("Glider Engine"),
-                resolution: (1280, 720).into(),
-                present_mode: bevy::window::PresentMode::Fifo,
+        .add_plugins((
+            #[cfg(feature = "render")]
+            DefaultPlugins.set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: String::from("Glider Engine"),
+                    resolution: (1280, 720).into(),
+                    present_mode: bevy::window::PresentMode::Fifo,
+                    ..default()
+                }),
                 ..default()
             }),
-            ..default()
-        }))
-        .add_plugins(core::CorePlugin)
+            core::CorePlugin,
+        ))
         .run();
 }
