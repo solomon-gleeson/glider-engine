360dc0a57091a7df1954b28fb8756f0bced84a65
solomon-gleeson <solomongleeson@pm.me>
2026-06-03 18:41:39 +0100

    add physics module

---
+++ src/core/physics.rs
@@ -0,0 +1,47 @@
+use avian2d::prelude::*;
+use bevy::prelude::*;
+
+pub struct PhysicsPlugin;
+
+impl Plugin for PhysicsPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_plugins(PhysicsPlugins::default())
+            .insert_resource(Gravity(Vec2::new(0.0, -980.0)))
+            .add_systems(Startup, setup_physics_test);
+    }
+}
+
+#[derive(Component)]
+pub struct Player;
+
+fn setup_physics_test(mut commands: Commands) {
+    commands.spawn((
+        SpriteBundle {
+            sprite: Sprite {
+                color: Color::srgb(0.3, 0.5, 0.3),
+                custom_size: Some(Vec2::new(800.0, 50.0)),
+                ..default()
+            },
+            transform: Transform::from_xyz(0.0, -200.0, 0.0),
+            ..default()
+        },
+        RigidBody::Static,
+        Collider::rectangle(800.0, 50.0),
+    ));
+
+    commands.spawn((
+        SpriteBundle {
+            sprite: Sprite {
+                color: Color::srgb(0.8, 0.2, 0.2),
+                custom_size: Some(Vec2::new(50.0, 50.0)),
+                ..default()
+            },
+            transform: Transform::from_xyz(0.0, 200.0, 0.0),
+            ..default()
+        },
+        RigidBody::Dynamic,
+        Collider::rectangle(50.0, 50.0),
+        LockedAxes::ROTATION_LOCKED,
+        Player,
+    ));
+}
